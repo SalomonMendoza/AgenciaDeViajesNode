d57169c29dbdb85981d85306d5f086a05b3d1ca6
Salomón Mendoza Montoya <salomendoza97@outlook.com>
2023-08-23 20:23:22 -0600

    primer commit

---
+++ models/Viaje.js
@@ -0,0 +1,30 @@
+import Sequelize, { DATE } from 'sequelize';
+import db from '../config/db.js'
+
+export const Viaje = db.define('viajes', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    precio: {
+        type: Sequelize.STRING 
+    },
+    fecha_ida: {
+        type: DATE
+    },
+    fecha_vuelta: {
+        type: DATE
+    },
+    imagen: {
+        type: Sequelize.STRING
+    },
+    descripcion: {
+        type: Sequelize.STRING
+    },
+    disponibles: {
+        type: Sequelize.STRING
+    },
+    slug: {
+        type: Sequelize.STRING
+    }
+
+});
